1d461f945161b985a38fa0738312918974a4299b
nitroclubseu <nitroclubseu@gmail.com>
2021-09-27 17:38:45 +0300

    Code refactoring, better comments, English version of instructions file

--- NITRObot_motor_calibration_simple/NITRObot_motor_calibration_simple.ino
+++ NITRObot_motor_calibration_simple/NITRObot_motor_calibration_simple.ino
@@ -1,48 +1,66 @@
-//Calibration test
+/*
+      NITRO Clubs EU - Network of IcT Robo Clubs
+ Site: https://www.nitroclubs.eu 
+ GitHub repositories https://github.com/nitroclubs?tab=repositories 
+ 
+  NITRObot motor speed calibration program
+
+ Use this sketch to calibrate the motor speeds of your NITRObot. 
+ You should fine tune your robot to move in a straight line.
+ Find the detailed instructions in NITRObot_motor_calibration-Instructions_EN.odt
+*/
+
+//====== INCLUDE ======
 #include <Arduino.h>
 
-#define LEFT_FOR 9    // PWMB
-#define LEFT_BACK 5   // DIRB  ---  Left
-#define RIGHT_FOR 6   // PWMA
-#define RIGHT_BACK 10 // DIRA  ---  Right
+//====== DEFINE ======
+#define MOTOR_LEFT_FWD_PIN 9    
+#define MOTOR_LEFT_BKWD_PIN 5   
+#define MOTOR_RIGHT_FWD_PIN 6   
+#define MOTOR_RIGHT_BKWD_PIN 10 
 
-const int LeftSpeed =100; // задава се скоростта на левия двигател при Калибриране
-const int RightSpeed =100; // задава се скоростта на десния двигател при Калибриране
-// Данните се запазват за задачите "Изход лабиринт" и "Следене на линия"
+//====== VARIABLES ======
+int speedLeft = 100;  // Initial  value for the speed of the left side motors 
+int speedRight = 100; // Initial  value for the speed of the right side motors
 
-int speedLeft = LeftSpeed;
-int speedRight = RightSpeed;
-//-----------------------------------------------
+//====== FORWARD DECLARATIONS ======
+void moveForward(); // function prototype includes return type, name, parameters, and semicolon.  No function body!
+void stopMoving();  // function prototype includes return type, name, parameters, and semicolon.  No function body!
+
+//----------------------------------------
 
 void setup()
 { 
-  pinMode(LEFT_FOR, OUTPUT);
-  pinMode(LEFT_BACK, OUTPUT);
-  pinMode(RIGHT_FOR, OUTPUT);
-  pinMode(RIGHT_BACK, OUTPUT);
+  pinMode(MOTOR_LEFT_FWD_PIN, OUTPUT);
+  pinMode(MOTOR_LEFT_BKWD_PIN, OUTPUT);
+  pinMode(MOTOR_RIGHT_FWD_PIN, OUTPUT);
+  pinMode(MOTOR_RIGHT_BKWD_PIN, OUTPUT);
 }
 
-//---------------------------------------------------------
+//----------------------------------------
 
 void loop()
 { 
-  moveForward();   
+  moveForward();
+  delay(5000); // With this default value, the robot will move for 5 seconds. 
+               // Change this based on the free space in front of the robot! 
+  stopMoving();   
 }
 
-//==================================== FUNCTIONS =====================================================
+//============== FUNCTION DEFINITIONS ==============
 
 void moveForward() // Move forward
 {
-  analogWrite(LEFT_FOR, abs(speedLeft));
-  analogWrite(LEFT_BACK, LOW);
-  analogWrite(RIGHT_FOR, abs(speedRight));
-  analogWrite(RIGHT_BACK, LOW);
+  analogWrite(MOTOR_LEFT_FWD_PIN, abs(speedLeft));
+  analogWrite(MOTOR_LEFT_BKWD_PIN, LOW);
+  analogWrite(MOTOR_RIGHT_FWD_PIN, abs(speedRight));
+  analogWrite(MOTOR_RIGHT_BKWD_PIN, LOW);
 }
 
 void stopMoving() // Stop movement
 {
-  analogWrite(LEFT_FOR, HIGH);
-  analogWrite(LEFT_BACK, HIGH);
-  analogWrite(RIGHT_FOR, HIGH);
-  analogWrite(RIGHT_BACK, HIGH);
+  analogWrite(MOTOR_LEFT_FWD_PIN, HIGH);
+  analogWrite(MOTOR_LEFT_BKWD_PIN, HIGH);
+  analogWrite(MOTOR_RIGHT_FWD_PIN, HIGH);
+  analogWrite(MOTOR_RIGHT_BKWD_PIN, HIGH);
 }
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-NITRObot_motor_calibration_simple
+
+					NITRObot_motor_calibration_simple
+
+The sketch (ino file) is located in  NITRObot_motor_calibration_simple folder of this repository.
+
+Select "Arduino Mega or Mega 2560" in the Arduino IDE and run the sketch.
+ 
+You can run the same source code in Visual Studio Code editor with PlatformIO extention installed,
+by replacing the "ino" file extention with "cpp" file extension and selecting Arduino Mega board for the project.
+
+NOTE: The Accompanying Documents and Training Materials in this repository are in:
+
+.odt OpenDocument Text and .ods - OpenDocument Spreadsheet
+
+file formats.
+
+You can open them with any package/program supporting the OpenDocument format. (i.e. LibreOffice, Microsoft Office /MS Word and MS Excell/ and other pakckages)
